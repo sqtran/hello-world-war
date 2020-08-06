8c8fe6f98e5ac7ddec2d4354562f6130f5644637
Steve Tran <stran@redhat.com>
2020-08-06 01:15:46 -0400

    Add forgotten class

---
+++ src/main/java/com/example/SampleClass.java
@@ -0,0 +1,21 @@
+package com.example;
+
+// Example from https://mkyong.com/maven/maven-pmd-example/
+public class SampleClass {
+    //Unused field
+    private int abc;
+
+    private String ip = "127.0.0.1";
+
+    public void test() {
+
+        String[] field = {"a", "b", "c", "s", "e"};
+
+        String s = "";
+        for (int i = 0; i < field.length; ++i) {
+            s = s + field[i];
+        }
+
+        System.out.println(ip);
+    }
+}
