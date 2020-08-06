b0aa549bec067f91fe7fc7e78f191ba198129ace
Steve Tran <stran@redhat.com>
2020-08-05 21:25:59 -0400

    Add sample test class

---
+++ src/test/java/com/example/SampleTest.java
@@ -0,0 +1,21 @@
+package com.example;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Just a dummy test class
+ */
+public class SampleTest {
+
+    @Test
+    public void testTrue() { 
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testFalse() { 
+        assertFalse(false);
+    }
+}
